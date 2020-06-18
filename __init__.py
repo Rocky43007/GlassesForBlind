fcbf2b73e1112c0beca24c73ae2c0d762077817c
Rocky43007 <arnabfromtks@gmail.com>
2020-06-18 16:33:47 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,33 +1,40 @@
 import pip
 import __Installer__.py
-import SensorData.py as sd
+import DistanceScanner.py as D
+import detect_realtime_tinyyolo_ncs.py as D2
 import OCRRecog.py
 
 
-def pipinstall():
-    try:
-        check_call(['sudo','apt-get', 'install', '-y', 'python3-pip'], stdout=open(os.devnull,'wb'))
-    except CalledProcessError as e:
-        print(e.output)
-
-def TesseractInstall():
-    try:
-        check_call(['sudo','apt-get', 'install', '-y', 'tesseract-ocr'], stdout=open(os.devnull,'wb'))
-    except CalledProcessError as e:
-        print(e.output)
-
-def install(package):
-    if hasattr(pip, 'main'):
-        pip.main(['install', package])
-    else:
-        pip._internal.main(['install', package])
-
+#def pipinstall():
+#    try:
+#        check_call(['sudo','apt-get', 'install', '-y', 'python3-pip'], stdout=open(os.devnull,'wb'))
+#    except CalledProcessError as e:
+#        print(e.output)
+#
+#def TesseractInstall():
+#    try:
+#        check_call(['sudo','apt-get', 'install', '-y', 'tesseract-ocr'], stdout=open(os.devnull,'wb'))
+#    except CalledProcessError as e:
+#        print(e.output)
+#
+#def install(package):
+#    if hasattr(pip, 'main'):
+#        pip.main(['install', package])
+#    else:
+#        pip._internal.main(['install', package])
+#
 # Example
-if __name__ == '__main__': 
-    pipinstall()
-    TesseractInstall()
-    install('numpy')    
+#if __name__ == '__main__': 
+#    pipinstall()
+#    TesseractInstall()
+#    install('numpy')    
+#
+#def Data():
+#    sd.ImageRec()
+#    sd.ultrasonic()
+name1 = input("Do you want to test? y/n: ")
 
-def Data():
-    sd.ImageRec()
-    sd.ultrasonic()
+if name1 == 'y':
+    D.Ultra()
+    D.Detect()
+    
